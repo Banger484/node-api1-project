ea3c2b8f2fad265ae1df238aad85854f390cf46d
Bret Banger <banger484@gmail.com>
2022-01-10 18:11:23 -0600

    initial setup

--- api/server.js
+++ api/server.js
@@ -1,3 +1,22 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const { find,
+     findById,
+     insert, 
+     update, 
+     remove, 
+     resetDB} = require("./users/model")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.get("/api/users", async (req, res) => {
+    try{
+        
+    }catch{
+
+    }
+})
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
